b468ad0f1b13cd8364435899be85b7400d351483
lchb369 <lchb@qq.com>
2016-02-29 05:07:40 -0500

    提交测试代码

--- test/test4.c
+++ test/test4.c
@@ -2,49 +2,44 @@
 #include <stdio.h>
 #include <stddef.h>
 
-typedef struct _header_data_pos_t
+
+typedef struct
 {
-	int server;
-	int date;
-	int content_length;
-	int content_encoding;
-	int location;
-	int last_modified;
-	int accept_ranges;
-	int expires;
-	int cache_control;
-	int etag;
+	int pos;
+	int length;
+}header_value_t;
+
+typedef struct 
+{
+	header_value_t server;
+	header_value_t date;
 }header_data_pos_t;
 
-/*
-int header_name_pos[] = {
-	{ "Server" , offsetof( header_data_pos_t , server )  },
-	{ "Date" , offsetof( header_data_pos_t , date )  },
-	
-};
-
-*/
-typedef struct _header_out_t
+typedef struct
 {
-	header_data_pos_t data_pos;
-	char data[1024];
 	int count;
 	int length;
+	char data[4096];
 }header_out_t;
 
 
-
-void header_out_push(  header_out_t* header_out , int pos , char* data , int len )
+typedef enum
 {
-	memcpy( &header_out->data + header_out->length , data , len  );
+   header_server = 0,
+   header_date	  
+}header_key;
+
+header_value_t header_values_arr[2] = {0};
+
+
+void header_out_push(  header_out_t* header_out , int key ,  char* data , int len )
+{
+	header_values_arr[key].pos = header_out->length;
+	header_values_arr[key].length = len;
+	
+	memcpy( header_out->data + header_out->length , data , len  );
 	header_out->length += len;
 	header_out->count += 1;
-
-printf( "start pos=%d...\n" , pos );
-	
-	memcpy( &header_out->data_pos +  pos  ,  len , sizeof( len ));	
-
-printf( "end...\n");
 }
 
 int main()
@@ -53,9 +48,13 @@ int main()
 header_out_t  header_out;
 memset( &header_out , 0 , sizeof( header_out ));
 
-char* serve = "Server/1.1.0";
-header_out_push( &header_out , offsetof( header_data_pos_t , server ) , serve , strlen( serve )  );
+char* serv = "Server/1.1.0";
+char* date = "Data-1985";
 
-printf( "header_out length=%d,count=%d,data_pos=%d,data=%s \n" , header_out.length , header_out.count , &header_out.data_pos + offsetof( header_data_pos_t , server ) , header_out.data );
+header_out_push( &header_out , header_server , serv , strlen( serv )  );
+header_out_push( &header_out , header_date ,   date , strlen( date ) );
+
+
+printf( "header_out length=%d,count=%d,data=%s \n" , header_out.length , header_out.count , header_out.data );
 return 0;
 }
